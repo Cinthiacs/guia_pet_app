b3b48267b72b2c557e758dfeef281d9609b9cfff
Cinthia Cavalheiro <Cinthia>
2024-09-30 11:01:10 -0300

    botao saiba mais e fotos ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.viewpager:viewpager:1.0.0")
 }
--- app/src/main/java/com/example/mapapp/TelaSaibaMaisBemLocaoActivity.java
+++ app/src/main/java/com/example/mapapp/TelaSaibaMaisBemLocaoActivity.java
@@ -1,24 +1,47 @@
 package com.example.mapapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Handler;
 
 public class TelaSaibaMaisBemLocaoActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private CustomPagerAdapter mCustomPagerAdapter;
+
+    int[] mResources = {
+            R.drawable.bemlocao_img1,
+            R.drawable.bemlocao_img2,
+            R.drawable.bemlocao_img3,
+    };
+
+    private Handler handler = new Handler();
+    private int currentPage = 0;
+
+    private Runnable pageSwitcher = new Runnable(){
+        @Override
+        public void run() {
+            if (currentPage == mResources.length) {
+                currentPage = 0;
+            }
+            mViewPager.setCurrentItem(currentPage++, true);
+            handler.postDelayed(this, 3000);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_saiba_mais_bem_locao);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        mViewPager = findViewById(R.id.pager);
+        mCustomPagerAdapter = new CustomPagerAdapter(this, mResources);
+        handler.postDelayed(pageSwitcher, 3000);
+        mViewPager.setAdapter(mCustomPagerAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(pageSwitcher);
     }
 }
